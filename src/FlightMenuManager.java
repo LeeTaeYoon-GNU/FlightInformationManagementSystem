26b1a5b359ac619946c093fe6ffb9398390dcc82
user <user@DESKTOP-UCACGJ2>
2020-03-19 17:24:40 +0900

    Create a MENU

--- src/FlightMenuManager.java
+++ src/FlightMenuManager.java
@@ -1,10 +1,11 @@
-public class HelloWorld {
+public class FlightMenuManager {
 	public static void main(String[] args) {
-		System.out.println("Select one nuber between 1 - 5:");
-		System.out.println("1. Add Student");
-		System.out.println("2. Delete Student");
-		System.out.println("3. Edit Student");
-		System.out.println("4. View Student");
-		System.out.println("5. Exit");
+		System.out.println("----Select 1 - 6 of Flight Information Menu---- \n");
+		System.out.println("1. Upload Flight");
+		System.out.println("2. Delete Flight");
+		System.out.println("3. Edit Flight");
+		System.out.println("4. View All Flight");
+		System.out.println("5. Search Flight");
+		System.out.println("6. Exit");
 	}
 }
