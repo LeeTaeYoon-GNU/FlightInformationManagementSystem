3ad405107c4e530bc8516504f8054c9a7efaf751
user <user@DESKTOP-UCACGJ2>
2020-03-19 16:05:41 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Select one nuber between 1 - 5:");
+		System.out.println("1. Add Student");
+		System.out.println("2. Delete Student");
+		System.out.println("3. Edit Student");
+		System.out.println("4. View Student");
+		System.out.println("5. Exit");
+	}
+}
